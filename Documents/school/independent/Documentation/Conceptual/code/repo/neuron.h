2e3521e57bc643a38bc2f6c410c0143fd7e50f0d
johnwilliamblood <johnwilliamblood@gmail.com>
2014-09-22 15:56:40 -0400

    update Mon Sep 22 15:56:40 EDT 2014

--- Documents/school/independent/Documentation/Conceptual/code/repo/neuron.h
+++ Documents/school/independent/Documentation/Conceptual/code/repo/neuron.h
@@ -186,9 +186,9 @@ void synapses(int& i)
 		//front right
 		mydata.neuron[4].gates=(rand()%10);
 		//front left
-		mydata.neuron[5].gates=(rand()%10)+15;
+		mydata.neuron[5].gates=(rand()%10);
 		//back right
-		mydata.neuron[6].gates=(rand()%10);
+		mydata.neuron[6].gates=(rand()%10)+15;
 		//back left
 		mydata.neuron[7].gates=(rand()%10);
 
