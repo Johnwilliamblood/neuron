d103460f53771658af7e54f609bbe37d6fb8369c
johnwilliamblood <johnwilliamblood@gmail.com>
2014-09-22 12:19:26 -0400

    update Mon Sep 22 12:19:26 EDT 2014

--- Documents/school/independent/Documentation/Conceptual/code/repo/neuron.h
+++ Documents/school/independent/Documentation/Conceptual/code/repo/neuron.h
@@ -197,15 +197,15 @@ void synapses(int& i)
 		Direction neuron forward, inhibits sensor neurons in rear. */
 		if (mydata.neuron[0].fire==1)
 		{
-			mydata.neuron[6].gates=mydata.neuron[6].gates-15;
-			mydata.neuron[7].gates=mydata.neuron[7].gates-15;
+			mydata.neuron[6].gates=mydata.neuron[6].gates-30;
+			mydata.neuron[7].gates=mydata.neuron[7].gates-30;
 		}		
 
 		//Direction neuron reverse, inhibits sensor neurons in front.
 		if (mydata.neuron[1].fire==1)
 		{
-			mydata.neuron[4].gates=mydata.neuron[4].gates-15;
-			mydata.neuron[5].gates=mydata.neuron[5].gates-15;
+			mydata.neuron[4].gates=mydata.neuron[4].gates-30;
+			mydata.neuron[5].gates=mydata.neuron[5].gates-30;
 		}
 
 
