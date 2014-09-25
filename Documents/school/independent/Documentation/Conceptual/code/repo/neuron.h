e96f043fcbf42f0b2c85f4308841956e7a874cf1
johnwilliamblood <johnwilliamblood@gmail.com>
2014-09-25 13:04:21 -0400

    update Thu Sep 25 13:04:21 EDT 2014

--- Documents/school/independent/Documentation/Conceptual/code/repo/neuron.h
+++ Documents/school/independent/Documentation/Conceptual/code/repo/neuron.h
@@ -210,14 +210,14 @@ void synapses()
 		if (mydata.neuron[4].fire==1)
 		{
 			mydata.neuron[2].gates=mydata.neuron[2].gates+350;
-			//mydata.neuron[3].kgates=mydata.neuron[3].kgates+5;
+			mydata.neuron[3].kgates=mydata.neuron[3].kgates+5;
 		}
 
 		//front left sensor
 
 		if (mydata.neuron[5].fire==1)
 		{
-			//mydata.neuron[2].kgates=mydata.neuron[2].kgates+5;
+			mydata.neuron[2].kgates=mydata.neuron[2].kgates+5;
 			mydata.neuron[3].gates=mydata.neuron[3].gates+350;
 		}
 
@@ -226,14 +226,14 @@ void synapses()
 		if (mydata.neuron[6].fire==1)
 		{
 			mydata.neuron[2].gates=mydata.neuron[2].gates+350;
-			//mydata.neuron[3].kgates=mydata.neuron[3].kgates+5;
+			mydata.neuron[3].kgates=mydata.neuron[3].kgates+5;
 		}
 
 		//reverse left sensor
 
 		if (mydata.neuron[7].fire==1)
 		{
-			//mydata.neuron[2].kgates=mydata.neuron[2].kgates+5;
+			mydata.neuron[2].kgates=mydata.neuron[2].kgates+5;
 			mydata.neuron[3].gates=mydata.neuron[3].gates+350;
 		}
 /*		Front and Reverse Sensors
@@ -358,9 +358,9 @@ void ping()
 		//front leftt
 		mydata.neuron[5].gates=0;
 		//back right
-		mydata.neuron[6].gates=600;
+		mydata.neuron[6].gates=300;
 		//back left
-		mydata.neuron[7].gates=300;
+		mydata.neuron[7].gates=150;
 
 		//front center
 		mydata.neuron[8].gates=0;
