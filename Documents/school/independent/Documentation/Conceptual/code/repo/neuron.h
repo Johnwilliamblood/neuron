f26428baa0f562498bc1512a921269d6c05105f3
johnwilliamblood <johnwilliamblood@gmail.com>
2014-09-25 13:06:21 -0400

    update Thu Sep 25 13:06:21 EDT 2014

--- Documents/school/independent/Documentation/Conceptual/code/repo/neuron.h
+++ Documents/school/independent/Documentation/Conceptual/code/repo/neuron.h
@@ -209,7 +209,7 @@ void synapses()
 
 		if (mydata.neuron[4].fire==1)
 		{
-			mydata.neuron[2].gates=mydata.neuron[2].gates+350;
+			mydata.neuron[2].gates=mydata.neuron[2].gates+300;
 			mydata.neuron[3].kgates=mydata.neuron[3].kgates+5;
 		}
 
@@ -218,14 +218,14 @@ void synapses()
 		if (mydata.neuron[5].fire==1)
 		{
 			mydata.neuron[2].kgates=mydata.neuron[2].kgates+5;
-			mydata.neuron[3].gates=mydata.neuron[3].gates+350;
+			mydata.neuron[3].gates=mydata.neuron[3].gates+300;
 		}
 
 		//reverse right sensor
 
 		if (mydata.neuron[6].fire==1)
 		{
-			mydata.neuron[2].gates=mydata.neuron[2].gates+350;
+			mydata.neuron[2].gates=mydata.neuron[2].gates+300;
 			mydata.neuron[3].kgates=mydata.neuron[3].kgates+5;
 		}
 
@@ -234,7 +234,7 @@ void synapses()
 		if (mydata.neuron[7].fire==1)
 		{
 			mydata.neuron[2].kgates=mydata.neuron[2].kgates+5;
-			mydata.neuron[3].gates=mydata.neuron[3].gates+350;
+			mydata.neuron[3].gates=mydata.neuron[3].gates+300;
 		}
 /*		Front and Reverse Sensors
 ------------------------------------------------------------------------------------
