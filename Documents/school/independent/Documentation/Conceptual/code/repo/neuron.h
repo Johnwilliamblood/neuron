d8b99eca8fc3464bcdd85926d35808373b3049c4
johnwilliamblood <johnwilliamblood@gmail.com>
2014-09-26 20:31:11 -0400

    update Fri Sep 26 20:31:11 EDT 2014

--- Documents/school/independent/Documentation/Conceptual/code/repo/neuron.h
+++ Documents/school/independent/Documentation/Conceptual/code/repo/neuron.h
@@ -300,7 +300,7 @@ void ping()
 		//back right
 		mydata.neuron[6].gates=200;
 		//back lef5
-		mydata.neuron[7].gates=90;
+		mydata.neuron[7].gates=88;
 
 		//front cent15
 		mydata.neuron[8].gates=0;
