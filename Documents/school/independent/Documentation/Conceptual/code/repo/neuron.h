23c36a41e6bf9bc26aff27271ddd5ba20fa36f0f
johnwilliamblood <johnwilliamblood@gmail.com>
2014-09-26 20:12:57 -0400

    update Fri Sep 26 20:12:57 EDT 2014

--- Documents/school/independent/Documentation/Conceptual/code/repo/neuron.h
+++ Documents/school/independent/Documentation/Conceptual/code/repo/neuron.h
@@ -161,14 +161,14 @@ void synapses()
 		if (mydata.neuron[4].fire==1)
 		{
 			mydata.neuron[2].gates=mydata.neuron[2].gates+200;
-			mydata.neuron[3].kgates=mydata.neuron[3].kgates+50;
+			mydata.neuron[3].kgates=mydata.neuron[3].kgates+25;
 		}
 
 		//front left sensor
 
 		if (mydata.neuron[5].fire==1)
 		{
-			mydata.neuron[2].kgates=mydata.neuron[2].kgates+50;
+			mydata.neuron[2].kgates=mydata.neuron[2].kgates+25;
 			mydata.neuron[3].gates=mydata.neuron[3].gates+200;
 		}
 		//reverse right sensor
@@ -176,14 +176,14 @@ void synapses()
 		if (mydata.neuron[6].fire==1)
 		{
 			mydata.neuron[2].gates=mydata.neuron[2].gates+200;
-			mydata.neuron[3].kgates=mydata.neuron[3].kgates+50;
+			mydata.neuron[3].kgates=mydata.neuron[3].kgates+25;
 		}
 
 		//reverse left sensor
 
 		if (mydata.neuron[7].fire==1)
 		{
-			mydata.neuron[2].kgates=mydata.neuron[2].kgates+50;
+			mydata.neuron[2].kgates=mydata.neuron[2].kgates+25;
 			mydata.neuron[3].gates=mydata.neuron[3].gates+200;
 		}
 /*		Front and Reverse Sensors
@@ -286,8 +286,6 @@ void check(int& i)
 }
 
 
-
-
 /*Functions
 -----------------------------------------------------------------------------------------
 
@@ -304,7 +302,7 @@ void ping()
 		//back lef2t
 		mydata.neuron[7].gates=250;
 
-		//front cente50
+		//front center
 		mydata.neuron[8].gates=0;
 		//back center
 		mydata.neuron[9].gates=160;
