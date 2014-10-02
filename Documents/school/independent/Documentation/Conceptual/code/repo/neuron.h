7692d230021d65b34ad76732510b99030552f1b0
johnwilliamblood <johnwilliamblood@gmail.com>
2014-10-02 11:26:34 -0400

    update Thu Oct  2 11:26:34 EDT 2014

--- Documents/school/independent/Documentation/Conceptual/code/repo/neuron.h
+++ Documents/school/independent/Documentation/Conceptual/code/repo/neuron.h
@@ -413,7 +413,7 @@ void fire()
 	}
 	if(mydata.neuron[2].fire==0)
 	{
-		if(speeda>75) {speeda=speeda-.05;}
+		if(speeda>75) {speeda=speeda-.01;}
 	}
 
 
@@ -425,7 +425,7 @@ void fire()
 	}
 	if(mydata.neuron[3].fire==0)
 	{
-		if(speedb>75) {speedb=speedb-.05;}
+		if(speedb>75) {speedb=speedb-.01;}
 	}
 
 
