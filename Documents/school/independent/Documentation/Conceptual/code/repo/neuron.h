cd77491ddd13e0e8f2994d6c7ee255185ae093cf
johnwilliamblood <johnwilliamblood@gmail.com>
2014-10-17 19:11:44 -0400

    update Fri Oct 17 19:11:44 EDT 2014

--- Documents/school/independent/Documentation/Conceptual/code/repo/neuron.h
+++ Documents/school/independent/Documentation/Conceptual/code/repo/neuron.h
@@ -88,7 +88,7 @@ void init_neuron_data()
 	for (int i=0;i<(number_of_neurons+1);i++)
 	{ 
 		mydata.neuron[i].id=i;
-		mydata.neuron[i].atp=999999999999999999;
+		mydata.neuron[i].atp=99;
 		mydata.neuron[i].fire=0;
 		mydata.neuron[i].NAgates=0;
 		mydata.neuron[i].kgates=0;
