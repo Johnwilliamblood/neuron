862c20490c1636ca2566009e6e662651afab975d
johnwilliamblood <johnwilliamblood@gmail.com>
2014-10-18 21:18:46 -0400

    update Sat Oct 18 21:18:46 EDT 2014

--- Documents/school/independent/Documentation/Conceptual/code/repo/arduino.cpp
+++ Documents/school/independent/Documentation/Conceptual/code/repo/arduino.cpp
@@ -170,7 +170,7 @@ void init_neuron_data()
   for (int i=0;i<number_of_neurons;i++)
   { 
     mydata.neuron[i].id=i;
-    mydata.neuron[i].atp=9999999;
+    mydata.neuron[i].atp=999999999999;
     mydata.neuron[i].state=1;
     mydata.neuron[i].fire=0;
     mydata.neuron[i].NAgates=0;
